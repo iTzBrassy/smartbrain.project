43e0231415cbbe889b4eb303ce124d63716c539b
iTzBrassy <itzbrassy@gmail.com>
2022-06-23 08:40:21 -0700

    App.js Restructure

--- src/App.js
+++ src/App.js
@@ -1,108 +1,120 @@
-import React from "react";
+import React, { Component } from "react";
 import Navigation from "./Components/Navigation/Navigation.js";
 import Logo from "./Components/Logo/Logo.js";
 import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm.js";
-import Rank from "./Components/Rank/Rank.js"
+import Rank from "./Components/Rank/Rank.js";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import "./App.css";
 
+const particlesInit = async (main) => {
+  console.log(main);
 
-function App() {
-  const particlesInit = async (main) => {
-    console.log(main);
+  // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+  // starting from v2 you can add only the features you need reducing the bundle size
+  await loadFull(main);
+};
 
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(main);
-  };
+const particlesLoaded = (container) => {
+  console.log(container);
+};
 
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (e) => {
+    console.log(e.target.value)
+  }
+  render() {
     return (
       <div className="App">
-       <Particles
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      options={{
-        fpsLimit: 30,
-        interactivity: {
-          events: {
-            onClick: {
-              enable: true,
-              mode: "push",
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            fpsLimit: 30,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                push: {
+                  quantity: 10,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
             },
-            onHover: {
-              enable: true,
-              mode: "repulse",
+            particles: {
+              color: {
+                value: "#ffffff",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              collisions: {
+                enable: true,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outModes: {
+                  default: "bounce",
+                },
+                random: false,
+                speed: 2,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                value: { min: 1, max: 5 },
+              },
             },
-            resize: true,
-          },
-          modes: {
-            push: {
-              quantity: 10,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: "#ffffff",
-          },
-          links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outModes: {
-              default: "bounce",
-            },
-            random: false,
-            speed: 2,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 800,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            value: { min: 1, max: 5 },
-          },
-        },
-        detectRetina: true,
-      }}
-    />
+            detectRetina: true,
+          }}
+        />
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} />
         {/* <FaceRecognition /> */}
       </div>
     );
   }
-
+}
 
 export default App;
--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange }) => {
   return (
     <div>
       <p className="f3">
@@ -8,7 +8,7 @@ const ImageLinkForm = () => {
       </p>
       <div className="center">
         <div className="center pa4 br3 shadow-5">
-          <input className="f4 pa2 w-70 center ba b--white" type="text" />
+          <input className="f4 pa2 w-70 center ba b--white" type="text" onChange={onInputChange}/>
           <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-blue ba b--light-blue">
             Detect
           </button>
